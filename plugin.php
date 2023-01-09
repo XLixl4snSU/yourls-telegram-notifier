de5a99c788657097a5afa3f0f5ad75a1f780f923
XLixl4snSU <111450124+XLixl4snSU@users.noreply.github.com>
2023-01-09 20:55:22 +0100

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Your Plugin Name
+Plugin URI: https://github.com/your_name/your_plugin
+Description: One line description of your plugin
+Version: 1.0
+Author: Your Name
+Author URI: https://your-site-if-any/
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+/*
+
+ Your code goes here.
+ 
+ Suggested read:
+ https://docs.yourls.org/guide/extend/plugins.html
+ https://docs.yourls.org/development/plugins.html
+ https://docs.yourls.org/development/coding-standards.html
+ and in general the whole "Development" section on https://docs.yourls.org/
+ 
+ Have fun!
+ 
+*/
